db60ff0d0f35cbaa9570c68bbd48b82c306ed761
vigilija <vigilija.simkeviciute@gmail.com>
2024-06-26 19:16:26 +0200

    Add project files.

---
+++ BankAPI/Account.cs
@@ -0,0 +1,9 @@
+﻿namespace BankAPI;
+
+public class Account
+{
+    public int Id { get; set; }
+    public long AcountNumber { get; set; }
+    public bool AccountOwnerId { get; set; }
+    public DateOnly OpenDate { get; set; }
+}
